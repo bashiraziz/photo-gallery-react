26e4fdad8efaf35ce6c73a0741573f1a011d3f1d
bashiraziz <bashiraziz@gmail.com>
2022-05-14 18:50:50 -0400

    Added Bismillah

--- src/comps/Title.js
+++ src/comps/Title.js
@@ -1,12 +1,22 @@
 import React from 'react';
+import './Title.css'
 
 const Title = () => {
   return (
+    <>    
+    <div className='center'>
+      <img src='https://firebasestorage.googleapis.com/v0/b/ramdhan2022-food-distribution.appspot.com/o/Other%2F2022-05-14%2018_31_30-bismillah%20al-rahman%20al-rahim%20-%20Google%20Search.png?alt=media&token=b60db3dc-a7e8-4111-89b7-2abf43f4da95' alt='Bismillah'></img>
+    </div>
     <div className="title">
       <h1>Ramdhan 2022 Food Ration Distribution</h1>
+      
+    <div>
       <h2> Success due to all the good hard work of Umar Kanjoli and team!!</h2>
       <h2>Pictures</h2>
+      </div>
     </div>
+    </>
+
   )
 }
 
