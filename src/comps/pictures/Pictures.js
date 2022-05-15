5c8c1af554a1874ac7848a7f99450279e4cbba96
bashiraziz <bashiraziz@gmail.com>
2022-05-15 03:36:31 -0400

    Adjusted Nav on Picture Page

--- src/comps/pictures/Pictures.js
+++ src/comps/pictures/Pictures.js
@@ -9,9 +9,7 @@ export default function Pictures() {
  const [selectedimg,setSelectedImg]=useState(null);
 return(
 <>
-   <div className='homepic'>
-      <img src='https://firebasestorage.googleapis.com/v0/b/ramdhan2022-food-distribution.appspot.com/o/Other%2F2022-05-14%2018_31_30-bismillah%20al-rahman%20al-rahim%20-%20Google%20Search.png?alt=media&token=b60db3dc-a7e8-4111-89b7-2abf43f4da95' alt='Bismillah'></img>
-    </div>
+   
 
 <ImageGrid setSelectedImg={setSelectedImg} />
 
