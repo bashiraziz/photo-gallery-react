946ab4fab3d53667809d3c862d052e3ca0879b5f
bashiraziz <bashiraziz@gmail.com>
2022-05-17 00:05:55 -0400

    Fixed title page

--- src/comps/Home.js
+++ src/comps/Home.js
@@ -9,9 +9,9 @@ export default function Home() {
   <Title />
   
   <div>
-      <p> A total amount of $10,637 was donated. May Allah reward abunduntly all the donors 
-          and all those who worked to make it a successul operation. 
-          Ration was distributed at 30 locations and 1100 beneficeries were served.
+      <p> A total amount of $10,637 was donated. May Allah reward abundantly all the donors 
+          and all those who worked to make it a successful operation. 
+          Ration was distributed at 30 locations and 1100 beneficiaries were served.
       </p>
     </div>
     <div className='homepic'>
